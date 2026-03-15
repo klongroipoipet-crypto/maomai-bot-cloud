8f6ccdeefe91e3b0dcd73f82a92a53ecaef1b984
klongroipoipet-crypto <klongroipoipet@gmail.com>
2026-03-15 12:26:31 +0700

    render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
-# ล้างตัวตัดบรรทัด Windows (\r) ออกจากไฟล์ requirements.txt ก่อนรัน
-sed -i 's/\r//g' requirements.txt
-
-# ติดตั้ง library
-pip install -r requirements.txt
+sed-i 's/\r//g' requirements.txt
+pip install-r requirements.txt
